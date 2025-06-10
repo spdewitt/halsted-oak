28762e78aac9d0ea3a7a9734485e21b4f234d1b7
spdewitt <spdewitt@outlook.com>
2025-06-10 14:48:34 -0500

    attempt 1

--- app/(sanity)/studio/[[...index]]/page.tsx
+++ app/(sanity)/studio/[[...index]]/page.tsx
@@ -1,19 +1,22 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio v3.
- * All routes under /studio will be handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+"use client";
 
-import Studio from './studio'
+// Instead of importing metadata and viewport from next-sanity,
+// we'll define them directly here
+export const metadata = {
+  title: "Sanity Studio",
+  description: "Content management for your site"
+};
 
-export const dynamic = 'force-static'
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1
+};
 
-export { metadata } from 'next-sanity/studio/metadata'
-export { viewport } from 'next-sanity/studio/viewport'
+// We only need the basic NextStudio component from next-sanity
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../sanity.config";
 
 export default function StudioPage() {
-  return <Studio />
+  return <NextStudio config={config} />;
 }
